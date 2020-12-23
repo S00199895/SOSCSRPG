20f9e8631bf723a1fe6193e04e5363a1cc104efc
Mark Curran <S00199895@mail.itsligo.ie>
2020-12-23 16:43:10 +0000

    Completed 04.3 Creating the Game World

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -11,7 +11,19 @@ namespace Engine.Factories
     {
         internal World CreateWorld()
         {
-            return new World();
+            World newWorld = new World();
+
+            newWorld.AddLocation(-2, -1, "Farmer's Field",
+                "There are rows of corn growing here, with giant rats hiding between them.",
+                "/Engine;component/Images/Locations/FarmFields.png");
+
+            newWorld.AddLocation(-1, -1, "Farmer's House",
+                "This is the house of your neighbour, Farmer Ted",
+                "/Engine;component/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(0, -1, "Home", "This is your house.", "/Engine;component/Images/Locations/Home.png");
+
+            return newWorld;
         }
     }
 }
